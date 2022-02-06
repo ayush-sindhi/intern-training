c4cce76c978fc932876c3a0c53fbd582c76b1963
Ayush Sindhi <ayush.sindhi@ibm.com>
2022-02-06 19:14:49 +0530

    2nd week

---
+++ day9/src/index.ts
@@ -0,0 +1,31 @@
+//implementation 1
+// let num: (string | number)[] = [1, 3, 4, 6, 7];
+// num.push("hello");
+// console.log(num);
+// let obj: {
+//   fname: string;
+//   lname: string;
+//   age: number;
+// };
+// obj = {
+//   fname: "john",
+//   lname: "dage",
+//   age: 28
+// };
+// console.log(obj);
+// let tup: [string, number, string?, number?];
+// tup = ["hello", 1, 2];
+// console.log(tup);
+// let anyType: any;
+// anyType = 2;
+// anyType = "hello";
+// function fun(a) {
+//   console.log(a);
+// }
+// fun(anyType);
+
+// implementation 2
+// let a: number = 80;
+// let b: number = 40;
+// if (a > b) console.log(`a is greater`);
+// else console.log(`b is greater`);
